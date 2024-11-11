d93e475004a5ba930ecb800ea5d27650fc51afad
Frederik Zwilling <frederik.zwilling@gmail.com>
2024-11-11 14:21:12 +0100

    deployment script

--- script/VoucherNFT.s.sol
+++ script/VoucherNFT.s.sol
@@ -13,11 +13,11 @@ contract VoucherNFTScript is Script {
         vm.startBroadcast();
 
         nft = new VoucherNFT(
-            "Voucher NFT",
-            "VOUCHER",
-            "https://voucher.com/voucher",
-            10,
-            address(this)
+            "Galactica Community Voucher - 1000 GNET",
+            "GCV1000",
+            "https://quicknode.quicknode-ipfs.com/ipfs/QmT9mHQcZsirYYsTpLBS9H4bZ1t3p6ZxiT9W4F3QWvRd8C",
+            1000,
+            0x46CD33b489D31E63eD722263A2397DbAEcb01FF8
         );
 
         vm.stopBroadcast();
